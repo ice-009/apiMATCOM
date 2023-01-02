d3f59be017ebf782cf765086e44f04e00c7bd9fa
ice-009 <21bma009@nith.ac.in>
2023-01-02 15:57:02 +0530

    auth successfully added

--- index.js
+++ index.js
@@ -3,14 +3,16 @@ const app = express();
 const port = process.env.PORT || 3000;
 const path = require("path");
 const mongoose = require("mongoose");
+const bcrypt= require('bcrypt');
+const User= require('./models/auth')
 const methodOverride = require("method-override");
 const { MongoClient } = require("mongodb");
-
+const auth= require('./models/auth')
 const Blog = require("./models/blog");
 const url = "mongodb+srv://ice-009:Armaan%4006@cluster0.ynzphiq.mongodb.net/";
 const databaseName = "apiMatcom";
+const session = require('express-session')
 const client = new MongoClient(url);
-
 async function dbConnect() {
   let result = await client.connect();
   db = result.db(databaseName);
@@ -31,13 +33,48 @@ mongoose
   });
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
-
+app.use(session({secret:'thisisasecret'}))
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
-
+const requireLogin= (req,res,next)=>{
+  if(!req.session.user_id){
+    res.redirect('/login');
+  }
+  next();
+}
 const categories = [];
 
-app.get("/blogs", async (req, res) => {
+
+// app.get('/register', (req,res)=>{
+//   res.render('blogs/register');
+// })
+app.post('/register', async (req,res)=>{
+  const {username, password} = req.body;
+  const hash=  await bcrypt.hash(password, 12)
+  const user = new User({
+    username,
+    password:hash
+  })
+  await user.save();
+  res.redirect('/');
+})
+app.get('/login', (req,res)=>{
+  res.render('blogs/login');
+})
+app.post('/login', async(req,res)=>{
+  const {username, passoword} = req.body;
+  const user= await User.findOne({username})
+  const validPass = bcrypt.compare(passoword, user.passoword)
+  if (validPass){
+    req.session._id= user._id
+    res.send('hi')
+  }
+  else {
+    res.send('try again')
+  }
+})
+
+app.get("/blogs", requireLogin, async (req, res) => {
   const { category } = req.query;
   if (category) {
     const blogs = await Blog.find({ category });
@@ -48,29 +85,29 @@ app.get("/blogs", async (req, res) => {
   }
 });
 
-app.get("/blogs/new", (req, res) => {
+app.get("/blogs/new", requireLogin,(req, res) => {
   res.render("blogs/new", { categories });
 });
 
-app.post("/blogs", async (req, res) => {
+app.post("/blogs",requireLogin, async (req, res) => {
   const newBlog = new Blog(req.body);
   await newBlog.save();
   res.redirect(`/blogs/${newBlog._id}`);
 });
 
-app.get("/blogs/:id", async (req, res) => {
+app.get("/blogs/:id",requireLogin, async (req, res) => {
   const { id } = req.params;
   const blog = await Blog.findById(id);
   res.render("blogs/show", { blog });
 });
 
-app.get("/blogs/:id/edit", async (req, res) => {
+app.get("/blogs/:id/edit", requireLogin,async (req, res) => {
   const { id } = req.params;
   const blog = await Blog.findById(id);
   res.render("blogs/edit", { blog, categories });
 });
 
-app.put("/blogs/:id", async (req, res) => {
+app.put("/blogs/:id",requireLogin, async (req, res) => {
   const { id } = req.params;
   const blog = await Blog.findByIdAndUpdate(id, req.body, {
     runValidators: true,
@@ -79,7 +116,7 @@ app.put("/blogs/:id", async (req, res) => {
   res.redirect(`/blogs/${blog._id}`);
 });
 
-app.delete("/blogs/:id", async (req, res) => {
+app.delete("/blogs/:id", requireLogin,async (req, res) => {
   const { id } = req.params;
   const deletedProduct = await Blog.findByIdAndDelete(id);
   res.redirect("/blogs");
